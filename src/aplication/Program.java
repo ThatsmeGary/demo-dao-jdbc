b0601ad008814ff40ac7f6faee5845beffb4fca2
Gary <gabriel.souza.dasilva@outlook.com>
2024-04-27 14:00:33 -0300

    Class Seller Add

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,11 +1,18 @@
 package aplication;
 
+import java.util.Date;
+
 import model.Department;
+import model.Seller;
 
 public class Program {
     public static void main(String[] args){
         
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(12,"Gary","gary@outlook.com", new Date(), 3200.0, obj);
+
         System.out.println(obj);
+        System.out.println(seller);
+
     }
 }
